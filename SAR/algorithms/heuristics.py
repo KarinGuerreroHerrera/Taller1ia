6af3bcf01f25dbece43333733a2b88046ea1138f
Victoria Ragua <m.ragua@uniandes.edu.co>
2026-02-22 12:38:59 -0500

    punto 5

--- SAR/algorithms/heuristics.py
+++ SAR/algorithms/heuristics.py
@@ -34,13 +34,45 @@ def survivorHeuristic(state: Tuple[Tuple, Any], problem: MultiSurvivorProblem):
     state: (position, survivors_grid)
     problem: MultiSurvivorProblem instance
 
-    This must be admissible and preferably consistent.
+    Version inicial del codigo(antes de usar la IA): 
+    Estaba usando mal el minimo y no tenia bien escrita la formula de la distancia
 
-    Hints:
-    - Use problem.heuristicInfo to cache expensive computations
-    - Go with some simple heuristics first, then build up to more complex ones
-    - Consider: distance to nearest survivor + MST of remaining survivors
-    - Balance heuristic strength vs. computation time (do experiments!)
+    position= state[0] 
+    survivors_grid= state[1]
+    position_surv = []
+
+    if survivors_grid.count()== 0:
+        return 0
+
+    for x in range(survivors_grid.width):
+        for y in range(survivors_grid.height):
+            if survivors_grid[x][y]:
+                position_surv.append((x, y))
+
+    for surv in position_surv:
+        dist = min(position[0] - surv[0] + position[1] - surv[1])
+
+    return dist
+
+    Prompts:
+    1. que es una heuristica y como puedo construir una para grid?
+    2. que es y como funciona la distancia manhatan y como se calcula para distintas posiciones
+    3. la formula para la distancia esta bien traducida en el codigo? dist = min(position[0]-surv[0] + position[1]-surv[1])
+    4. como probar la heuristica si tengo A* y layouts en el proyecto
     """
     # TODO: Add your code here
-    utils.raiseNotDefined()
+    position, survivors_grid= state
+    position_surv= []
+    if survivors_grid.count()== 0:
+        return 0
+    
+    for x in range(survivors_grid.width):
+        for y in range(survivors_grid.height):
+            if survivors_grid[x][y]:
+                position_surv.append((x,y))
+
+    min_dist= float('inf')
+    for surv in position_surv:
+        dist= abs(position[0]-surv[0])+ abs(position[1]-surv[1])
+        min_dist= min(min_dist, dist)
+    return min_dist
